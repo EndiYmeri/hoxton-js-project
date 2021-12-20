06220e09fcbb904fdc97c3d7993f74c06f216c3d
AdrianoRama <ramaadriano@outlook.com>
2021-12-20 18:56:23 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const state = {
     movies: [],
-    keyword: "latest"
+    keyword: "upcoming"
 }
 
 
@@ -9,7 +9,7 @@ const state = {
 
 function getData(keyword) {
 
-    return fetch(`http://api.themoviedb.org/3/movie/${keyword}?api_key=713b8a6c62fe6832204cde2d50900308`).then(function(resp) {
+    return fetch(`http://api.themoviedb.org/3/movie/${keyword}?api_key=713b8a6c62fe6832204cde2d50900308`).then(function (resp) {
         return resp.json()
     })
 }
@@ -24,7 +24,6 @@ function getPopularMovies() {
 
 function movieList() {
     const ulEl = document.createElement('ul')
-    ulEl.innerHTML = ``
 
     for (const movie of state.movies) {
         const movieLiEl = document.createElement('li')
@@ -35,7 +34,7 @@ function movieList() {
         movieLiEl.append(movieTitle)
         ulEl.append(movieLiEl)
     }
-    return ulEl
+    main.append(ulEl)
 }
 
 const header = document.createElement(`header`)
@@ -44,24 +43,25 @@ const main = document.createElement(`main`)
 
 const footer = document.createElement(`footer`)
 
+const body = document.querySelector(`body`)
+
 
 
 
 function render() {
 
-    document.body.innerHTML = ""
-    main.append(movieList())
-    document.body.append(header, main, footer)
+    body.innerHTML = ""
+    movieList()
+    body.append(header, main, footer)
+
 
 }
 
-
-render()
-
 function init() {
-    getData(state.keyword).then(function(item) {
-        state.movies = item
+    getData(state.keyword).then(function (item) {
+        state.movies = item.results
     })
     render()
 }
+
 init()
