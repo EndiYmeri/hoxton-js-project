596705232de7be1417fe5a2fc20fa575b6eb7d86
EndiYmeri <56412395+EndiYmeri@users.noreply.github.com>
2021-12-20 18:36:12 +0100

    Working on upcoming movies
    Co-authored-by: AdrianoRama <AdrianoRama@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const state = {
-    movies: []
+    movies: [],
+    keyword: "latest"
+}
+
+
+
+
+
+function getData(keyword) {
+
+    return fetch(`http://api.themoviedb.org/3/movie/${keyword}?api_key=713b8a6c62fe6832204cde2d50900308`).then(function(resp) {
+        return resp.json()
+    })
+}
+
+function getPopularMovies() {
+    let popular = "popular"
+    getData(popular)
+    state.keyword
+    render()
+}
+
+
+function movieList() {
+    const ulEl = document.createElement('ul')
+    ulEl.innerHTML = ``
+
+    for (const movie of state.movies) {
+        const movieLiEl = document.createElement('li')
+        const movieTitle = document.createElement('h2')
+
+        movieTitle.textContent = movie.title
+
+        movieLiEl.append(movieTitle)
+        ulEl.append(movieLiEl)
+    }
+    return ulEl
 }
 
 const header = document.createElement(`header`)
@@ -8,26 +44,24 @@ const main = document.createElement(`main`)
 
 const footer = document.createElement(`footer`)
 
-document.body.append(header, main, footer)
 
-function getData() {
-    return fetch('http://api.themoviedb.org/3/movie/popular?api_key=713b8a6c62fe6832204cde2d50900308').then(function (resp) {
-        return resp.json()
-    })
-}
 
-function getState() {
-    getData().then(function (item) {
-        state.movies = item
-        render()
-    })
-}
 
 function render() {
 
+    document.body.innerHTML = ""
+    main.append(movieList())
+    document.body.append(header, main, footer)
+
 }
 
-getState()
 
 render()
 
+function init() {
+    getData(state.keyword).then(function(item) {
+        state.movies = item
+    })
+    render()
+}
+init()
